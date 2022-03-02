a48e4ec206e0e8ca3b7902ab6ca4aa89cfa4bb30
Newton Urbanetz <new.urbanetz@gmail.com>
2022-03-02 21:53:48 +0100

    feat: add atividade 2

---
+++ 2_semestre/atividade_2/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// Crie um programa para imprimir seus dados, 
+// que devem ser apresentados na tela da seguinte maneira:
+// Nome: Regiane Marcon
+// Profissão: Professora
+// Cidade: Dourados
+// UF: MS
+// Utilize printf para desenvolver essa atividade. 
+int main()
+{
+    printf("Nome: Newton Urbanetz");
+    printf("\n");
+    printf("Profissão: programador");
+    printf("\n");
+    printf("Cidade: Curitiba");
+    printf("\n");
+    printf("UF: PR");
+
+    return 0;
+}
