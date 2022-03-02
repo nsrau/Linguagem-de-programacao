01a5146f6106335665b54dd2249c2f6fda1a4872
Newton Urbanetz <new.urbanetz@gmail.com>
2022-03-02 22:36:58 +0100

    refactor: update atividade 3 with setlocale

--- 2_semestre/atividade_3/main.c
+++ 2_semestre/atividade_3/main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
 
 // Crie um programa que leia os valores para 3 
 // variáveis do tipo int e mostre os valores depois de lidos.
 
 int main()
 {
+    setlocale(LC_ALL, "Portuguese");
+
     int numA, numB, numC;
 
     printf("por favor inserir o valor de numero a: ");
@@ -19,13 +23,13 @@ int main()
     scanf("%d", &numC);
     printf("\n");
 
-    printf("o valor do numero a é: %d", numA);
+    wprintf(L"o valor do numero a é: %d", numA);
     printf("\n");
 
-    printf("o valor do numero b é: %d", numB);
+    wprintf(L"o valor do numero b é: %d", numB);
     printf("\n");
 
-    printf("o valor do numero c é: %d", numC);
+    wprintf(L"o valor do numero c é: %d", numC);
 
     return 0;
 }
