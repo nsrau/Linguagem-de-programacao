2cbd1f311f814e10b5c3988d6d1d0ef0a6453684
Newton Urbanetz <new.urbanetz@gmail.com>
2022-03-02 21:29:29 +0100

    feat: init and add atividade 1

---
+++ 2_semestre/atividade_1/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// Crie um programa que contenha duas variáveis globais do tipo int que devem 
+// receber os valores 10 e 20, 
+// duas variáveis locais do tipo float que devem 
+// receber os valores 2.45 e 7.98 e uma constante do tipo int 
+// que deve receber o valor 100.
+
+int num1, num2;
+
+int main()
+{
+    num1 = 10;
+    num2 = 20;
+    float valor1, valor2;
+    valor1 = 2.45;
+    valor2 = 7.98;
+    const int valor = 100;
+    return 0;
+}
