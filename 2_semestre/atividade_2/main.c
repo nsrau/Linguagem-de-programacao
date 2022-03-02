dd707bd964b6aba6d59e87286a517408c1eca801
Newton Urbanetz <new.urbanetz@gmail.com>
2022-03-02 22:36:36 +0100

    refactor: update atividade 2 with setlocale

--- 2_semestre/atividade_2/main.c
+++ 2_semestre/atividade_2/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
 
 // Crie um programa para imprimir seus dados, 
 // que devem ser apresentados na tela da seguinte maneira:
@@ -9,9 +11,11 @@
 // Utilize printf para desenvolver essa atividade. 
 int main()
 {
+    setlocale(LC_ALL, "Portuguese");
+
     printf("Nome: Newton Urbanetz");
     printf("\n");
-    printf("Profissão: programador");
+    wprintf(L"Profissão: programador");
     printf("\n");
     printf("Cidade: Curitiba");
     printf("\n");
