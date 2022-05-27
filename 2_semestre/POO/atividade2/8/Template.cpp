5cbcd30e416c092659e04ab09f9e377cc2ebc25c
Newton Urbanetz <new.urbanetz@gmail.com>
2022-05-27 22:44:15 +0200

    feat: atividade 2 numero 8

--- 2_semestre/POO/atividade2/8/Template.cpp
+++ 2_semestre/POO/atividade2/8/Template.cpp
@@ -4,13 +4,5 @@
 template <typename T>
 T Maximo(T arg1, T arg2)
 {
-    if (arg1 > arg2)
-    {
-        return arg1;
-    }
-    if (arg2 > arg1)
-    {
-        return arg2;
-    }
-    return arg1;
+    return arg1>arg2?arg1:arg2;
 }
